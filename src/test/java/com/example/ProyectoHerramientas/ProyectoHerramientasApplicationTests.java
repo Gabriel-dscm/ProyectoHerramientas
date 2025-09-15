6130367761d223337db0aaa257eb653d1dd91033
Gasco <fgascorojas@gmail.com>
2025-09-15 12:49:04 -0500

    Proyecto Inicial con Spring Boot y gitignore

---
+++ src/test/java/com/example/ProyectoHerramientas/ProyectoHerramientasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProyectoHerramientas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoHerramientasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
